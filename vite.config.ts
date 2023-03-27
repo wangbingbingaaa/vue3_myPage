bce744092e6c7a5efbbcbf443ba1a0341acb2ad6
wangxb <444698971@qq.com>
2023-03-27 09:44:45 +0800

    up

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: './',
-    publicDir: 'public', // 静态资源服务的文件夹
-    plugins: [vue()],
+export default defineConfig(({command }) => {
+    let base= './';
+    if (command === 'build'){
+        base = './vue3_myPage/'
+    } else {
+        base = './'
+    }
+   
+    return {
+        plugins: [vue()],
+        publicDir: 'public', // 静态资源服务的文件夹
+
+    }
+ 
 })
